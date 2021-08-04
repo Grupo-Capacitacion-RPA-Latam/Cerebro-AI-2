1cfaa4ebcccf8c8e2d8bc1c102341757794a0706
isaiahcedano <isaiahcedano14@gmail.com>
2021-08-03 20:21:41 -0500

    Agregado responsiveness al introduccion. * Agregado useRef y useState al introduccion. * Editado introduccion.module.css.

--- src/Componentes/Introduccion/Introduccion.js
+++ src/Componentes/Introduccion/Introduccion.js
@@ -1,7 +1,15 @@
 import clases_css from './Introduccion.module.css';
-
+import {useRef, useState} from 'react';
 
 const Introduccion = ({titulo, descripcion, imagen, enlaceRedireccion}) => {
+    const img = useRef(null);
+    const [claseCss, setClaseCss] = useState("")
+
+    const getImgWidth = () => {
+        if (img.current.naturalWidth <= 450) {
+            setClaseCss(clases_css.introduccion__imagen_contenedor__imagen_especial)
+        }
+    };
     return (
         <div className={clases_css.introduccion}>
             <div className={clases_css.introduccion__titulo_descripcion_enlace}>
@@ -9,7 +17,9 @@ const Introduccion = ({titulo, descripcion, imagen, enlaceRedireccion}) => {
                 <p>{descripcion}</p>
                 <a href={enlaceRedireccion} className={clases_css.introduccion__enlace}>Ver mas</a>
             </div>
-            <img src={imagen} alt=""/>
+            <div className={clases_css.introduccion__imagen_contenedor}>
+                <img src={imagen} alt="" ref={img} onLoad={getImgWidth} className={claseCss}/>
+            </div>
         </div>
     );
 };
